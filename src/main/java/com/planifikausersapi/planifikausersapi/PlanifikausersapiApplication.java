8bd05dfa79a4af0290fbe42a0e48687d144148d8
Spdrd <juandiegoreyesrodriguez@gmail.com>
2025-10-09 13:10:06 -0500

    create spring project

---
+++ src/main/java/com/planifikausersapi/planifikausersapi/PlanifikausersapiApplication.java
@@ -0,0 +1,13 @@
+package com.planifikausersapi.planifikausersapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanifikausersapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanifikausersapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/planifikausersapi/planifikausersapi/PlanifikausersapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.planifikausersapi.planifikausersapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanifikausersapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
